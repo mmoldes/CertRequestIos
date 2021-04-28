974bd23a25f1bac43e41c257ee081e73f1072860
m.moldes <m.moldes@alumnos.upm.es>
2021-04-28 16:08:01 +0200

    Ahora al ejecutar EmbedIO devuelve Hola Mundo en el explorador.

--- ServerIOS/Program.cs
+++ ServerIOS/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-
+using System.Threading;
+using EmbedIO;
 namespace ServerIOS
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = "http://localhost:9696/";
+            if (args.Length > 0)
+                url = args[0];
+
+            // Create Webserver and attach LocalSession and Static
+            // files module and CORS enabled
+            WebServer server = new WebServer(o => o
+             .WithUrlPrefix(url)
+             .WithMode(HttpListenerMode.EmbedIO))
+             .WithLocalSessionManager()
+             .WithAction("/test", HttpVerbs.Any, ctx =>
+             {
+                 Console.WriteLine("Request received");
+                 return ctx.SendDataAsync(new { mensaje = "Hola Mundo!" });
+             }
+
+         );
+
+            var cts = new CancellationTokenSource();
+            var task = server.RunAsync(cts.Token);
+
+            Console.ReadKey(true);
+            cts.Cancel();
+
+            // Wait before dispose server
+            task.Wait();
+            server.Dispose();
         }
     }
 }
